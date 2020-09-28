b0bd9b4a7037a9f89827ca5d02671e9eaf6e5d9d
yuzhou <351975711@qq.com>
2020-09-28 17:24:09 +0800

    stat

--- src/main/java/org/legion/aegis/issuetracker/dao/IssueStatisticsDAO.java
+++ src/main/java/org/legion/aegis/issuetracker/dao/IssueStatisticsDAO.java
@@ -1,8 +1,12 @@
 package org.legion.aegis.issuetracker.dao;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.legion.aegis.common.base.SearchParam;
 import org.legion.aegis.issuetracker.entity.Issue;
+import org.legion.aegis.issuetracker.vo.UserStatisticsVO;
+
 import java.util.List;
 
 @Mapper
@@ -10,4 +14,6 @@ public interface IssueStatisticsDAO {
 
     @Select("SELECT * FROM ISU_ISSUE WHERE PROJECT_ID = #{projectId} ")
     List<Issue> retrieveIssueByProject(Long projectId);
+
+    List<UserStatisticsVO> searchUserStat(@Param("sp") SearchParam param);
 }
